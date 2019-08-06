afe8340cb1cb0dc42c7e6790281d8c179faa2f73
TribusK2 <biuro@tribusk2.pl>
2019-08-07 00:13:18 +0200

    finish

--- stage2.js
+++ stage2.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // load gallery
     var loadGallery = function(){
         $("#profilePage").fadeOut(200, function(){
             $("#loaderPage").fadeIn(200, function(){
@@ -14,5 +15,37 @@ $(document).ready(function(){
         };
     });
     $('#inputBtn').click(loadGallery);
-    
+    // load gallery end
+
+    // open chat
+    var openChat = function(){
+        $('#chatWindow').css('bottom', '5%');
+    };
+    $('.picMessage').click(openChat);
+    // open chat end
+
+    // chatting
+    var chating = function(){
+        var inputChat = $('#inputChat').val();
+        var kamilaWritingDeley = 3000;
+        var kamilaAnswerDeley = 3000;
+        var kamilaText = 'Cześć :)';
+        var kamilaTime = '13:18';
+        $('#slowText').html(inputChat);
+        $('#slowChating').fadeIn(100, function(){
+            $('#kamilaChating').delay(kamilaWritingDeley).fadeIn(200, function(){
+                $('.show').delay(kamilaAnswerDeley).fadeOut(100, function(){
+                    $('#kamilaText').html(kamilaText);
+                    $('#kamilaTime').html(kamilaTime);
+                    $('.show').fadeIn(100);
+                });  
+            });
+        });
+    };
+    $('#inputChat').on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            chating();
+        };
+    });
+    // chatting end
 });
